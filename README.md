d0e5321f8fd65218cb7238f9d40959ecc9b184ec
vladc <vldcnst@yahoo.com>
2015-01-22 01:54:20 +0200

    v0.2

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # WHMCS CAPTCHA
 ![captcha breaker](http://i.imgur.com/yrAPIgh.gif) 
 
-A Proof of Concept Firefox (>34) extension that breaks WHMCS.com's CAPTCHA, with 100% accuracy.
+A Proof of Concept Firefox (>34) extension that breaks WHMCS's CAPTCHA, with 100% accuracy.
 
-Works on http://www.whmcs.com/members/verifydomain.php
+Example: http://www.whmcs.com/members/verifydomain.php
+
+## Installation
+
+You can install by dragging the latest [whmcs_captcha.xpi](https://github.com/vladc/WHMCS-CAPTCHA/releases) file to your add-on page.
--- src/lib/main.js
+++ src/lib/main.js
@@ -1,5 +1,33 @@
-var pageMod = require("sdk/page-mod");
-pageMod.PageMod({
-	include: ["http://www.whmcs.com/members/verifydomain.php", "https://www.whmcs.com/members/verifydomain.php"],
-	contentScriptFile: "./captcha.js"
+const {ContentPolicy} = require("./policy");
+const {components} = require("chrome");
+var {modelFor} = require("sdk/model/core");
+var tabUtils = require("sdk/tabs/utils");
+var tabs = require("sdk/tabs");
+
+ContentPolicy({
+	description: "Intercepts images with includes/verifyimage.php in path",
+	shouldLoad: function(data) {
+		try {
+			if(data.image && data.location.path.substr(-24) == "includes/verifyimage.php") {
+				if (data.context instanceof components.interfaces.nsIDOMNode) {
+					var node = data.context.QueryInterface(components.interfaces.nsIDOMNode);
+					if ("ownerDocument" in node && node.ownerDocument) {
+						node = node.ownerDocument;
+					}
+					if ("defaultView" in node) {
+						node = node.defaultView;
+					}
+					if (node) {
+						var tab = modelFor(tabUtils.getTabForContentWindow(node));
+						tab.once("load", function(wtab) { 
+							wtab.attach({contentScriptFile: "./captcha.js"});
+						});
+					}
+				}
+			}
+		} catch(e) {
+			console.log("WHMCS-Captcha: " + e);
+		}
+		return true;
+	}
 });
